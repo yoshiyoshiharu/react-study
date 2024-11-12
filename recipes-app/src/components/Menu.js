f945b5f1b20688ec05dd85995f314a46803b3f8e
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-11-12 22:52:23 +0900

    workded

--- recipes-app/src/components/Menu.js
+++ recipes-app/src/components/Menu.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Recipe from './Recipe';
 
-export default function Menu({ recieps }) {
+export default function Menu({ recipes }) {
   return (
     <article>
       <header>
         <h1>Delicious Recipes</h1>
       </header>
       <div className='recipes'>
-        {recipes.map((recipe, i) => {
+        {recipes.map((recipe, i) => (
           <Recipe key={i} {...recipe} />
-        })}
+        ))}
       </div>
     </article>
   )
--- recipes-app/src/components/Recipe.js
+++ recipes-app/src/components/Recipe.js
@@ -2,11 +2,11 @@ import React from 'react';
 import IngredientList from './IngredientList';
 import Instructions from './Instructions';
 
-export default function Recipe({ name, ingerdients, steps }) {
+export default function Recipe({ name, ingredients, steps }) {
   return (
     <section id='baked-salmon'>
       <h1>{name}</h1>
-      <IngredientList ingredients={ingerdients} />
+      <IngredientList ingredients={ingredients} />
       <Instructions title='Cooking Instructions' steps={steps} />
     </section>
   )
