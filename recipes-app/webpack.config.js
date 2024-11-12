cd1e7ca184b810f798152e6cdbdc4ce43ed21c9b
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-11-12 22:29:02 +0900

    build

---
+++ recipes-app/webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist', 'assets'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+};
